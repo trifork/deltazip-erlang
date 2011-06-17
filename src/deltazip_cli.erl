e55700f06dc2432846d03cb3bfa1003fc01bb6cd
Erik Søe Sørensen <ess@trifork.com>
2011-06-17 14:52:09 +0200

    deltazip_cli: Added 'count' operation.

--- src/deltazip_cli.erl
+++ src/deltazip_cli.erl
@@ -9,10 +9,14 @@ main(L) ->
 interpret_command(["create", DZFile | InputFiles]) ->
     do_create(DZFile, InputFiles);
 interpret_command(["get", DZFile]) ->
-    do_get(DZFile).
+    do_get(DZFile);
+interpret_command(["count", DZFile]) ->
+    do_count(DZFile).
+%%%======================================================================
 
+%%%----------
 do_create(DZFile, InputFiles) ->
-    {ok, Fd} = file:open(DZFile, [write, exclusive, binary, raw]),
+    {ok, Fd} = file:open(DZFile, [write, exclusive, binary]),
     Access = fd_access(Fd),
     DZ = deltazip:open(Access),
     Datas = [begin {ok,D} = file:read_file(F), D end
@@ -22,14 +26,39 @@ do_create(DZFile, InputFiles) ->
     ok = file:write(Fd, Data),
     ok = file:close(Fd).
 
+%%%----------
 do_get(DZFile) ->
-    {ok, Fd} = file:open(DZFile, [read, binary, raw]),
+    {ok, Fd} = file:open(DZFile, [read, binary]),
     Access = fd_access(Fd),
     DZ = deltazip:open(Access),
     Data = deltazip:get(DZ),
     deltazip:close(DZ),
     ok = file:close(Fd),
     io:put_chars(Data). % TODO: Is non-ascii data handled correctly?
+
+%%%----------
+do_count(DZFile) ->
+    {ok, Fd} = file:open(DZFile, [read, binary]),
+    Access = fd_access(Fd),
+    DZ = deltazip:open(Access),
+    Cnt = count_entries(DZ),
+    deltazip:close(DZ),
+    ok = file:close(Fd),
+    io:format("~b\n", [Cnt]).
+
+count_entries(DZ) ->
+    case deltazip:get(DZ) of
+	file_is_empty -> 0;
+	_ -> count_entries(DZ, 1)
+    end.
+
+count_entries(DZ, Acc) ->
+    case deltazip:previous(DZ) of
+	{ok, DZ2} -> count_entries(DZ2, Acc+1);
+	{error, at_beginning} -> Acc
+    end.
+
+%%%======================================================================
 
 fd_access(Fd) ->
     GetSizeFun = fun() -> {ok, Pos} = file:position(Fd, eof), Pos end,
